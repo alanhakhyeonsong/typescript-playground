67ecf5ec163720b9cf3aee3d6126b24e19c08a59
alanhakhyeonsong <songs4805@naver.com>
2023-12-09 23:28:00 +0900

    Feat: section 2, 3

---
+++ class-note/1_type-basic.ts
@@ -0,0 +1,28 @@
+// TS 문자열 선언
+let str: string = 'hello';
+
+// TS 숫자
+let num: number = 10;
+
+// TS 배열
+let arr: Array<number> = [1, 2, 3];
+let names: Array<string> = ['Ramos', 'Kroos', 'Modric'];
+let items: number[] = [1, 2, 3];
+
+// TS 튜플
+let address: [string, number] = ['Seoul', 10];
+
+// TS 객체
+let obj: object = {};
+let person: object = {
+  name: 'Ramos',
+  age: 28
+};
+
+let person2: { name: string, age: number } = {
+  name: 'Ramos',
+  age: 28
+};
+
+// TS 진위값
+let show: boolean = true;
