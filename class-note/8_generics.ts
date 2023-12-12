1067a3a7100bf1781a15d48f472a5c9428df6df4
alanhakhyeonsong <songs4805@naver.com>
2023-12-12 13:05:16 +0900

    Feat: section 10

--- class-note/8_generics.ts
+++ class-note/8_generics.ts
@@ -16,3 +16,37 @@ interface Dropdown<T> {
 }
 
 const obj: Dropdown<number> = { value: 1, selected: false };
+
+// 제네릭의 타입 제한
+function logTextLength<T>(text: T[]): T[] {
+  console.log(text.length);
+  text.forEach(text => console.log(text));
+  return text;
+}
+logTextLength<string>(['hi', 'abc']);
+
+// 제네릭 타입 제한 2 - 정의된 타입 이용하기
+interface LengthType {
+  length: number;
+}
+
+function logTextLength2<T extends LengthType>(text: T): T {
+  console.log(text.length);
+  return text;
+}
+logTextLength2('a');
+logTextLength2({ length: 10 });
+
+// 제네릭의 타입 제한 3 - keyof
+interface ShoppingItem {
+  name: string;
+  price: number;
+  stock: number;
+}
+
+function getShoppingItemOption<T extends keyof ShoppingItem>(itemOption: T): T {
+  return itemOption;
+}
+// getShoppingItemOption(10);
+// getShoppingItemOption<string>('a');
+getShoppingItemOption('name');
