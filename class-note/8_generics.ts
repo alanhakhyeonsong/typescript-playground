9898ef8c9913e76d6ad40c0103b5cb7e5ea85018
alanhakhyeonsong <songs4805@naver.com>
2023-12-12 12:46:58 +0900

    Feat: section 10

--- class-note/8_generics.ts
+++ class-note/8_generics.ts
@@ -8,3 +8,11 @@ const str = logText<string>('abc');
 str.split('');
 
 const flag = logText<boolean>(true);
+
+// 인터페이스에 제네릭을 선언하는 방법
+interface Dropdown<T> {
+  value: T;
+  selected: boolean;
+}
+
+const obj: Dropdown<number> = { value: 1, selected: false };
